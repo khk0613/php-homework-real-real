ac79f99676a596820210be192b34f9823c39113e
hk <khk0613@cside.co.kr>
2015-12-01 08:41:30 +0900

    올라가랏

---
+++ board2/index.php
@@ -0,0 +1,42 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>자유게시판 | KHK</title>
+	<link rel="stylesheet" href="./css/normalize.css" />
+	<link rel="stylesheet" href="./css/board.css" />
+</head>
+<body>
+	<article class="boardArticle">
+		<h3>자유게시판1</h3>
+		<div id="boardList">
+			<form action="removephp1.php" method="post">
+				<div class="btnSet">
+					 <a href="./write.php" class="btnWrite btn">글쓰기</a>
+					<input type="submit" name="submit" value="remove">
+				</div>
+				<table>
+					<caption class="readHide">자유게시판1</caption>
+					<thead>
+						<tr>
+							<th scope="col" class="delete">체크박스</th>
+							<th scope="col" class="no">번호</th>
+							<th scope="col" class="title">제목</th>
+							<th scope="col" class="author">작성자</th>
+							<th scope="col" class="date">작성일</th>
+							<th scope="col" class="hit">조회</th>
+						</tr>
+					</thead>
+					<tbody>
+						
+					</tbody>
+				</table>
+				<div class="btnSet">
+					<a href="./write.php" class="btnWrite btn">글쓰기</a>
+				</div>
+			</form>
+		</div>
+	</article>
+</body>
+</html>
