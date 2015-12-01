0dbbf79623b28e7b58cd051ae3f0f8d508058384
khk <khk0613@cside.co.kr>
2015-12-01 09:21:33 +0900

    수정하였음

--- board2/index.php
+++ board2/index.php
@@ -3,7 +3,7 @@
 <html>
 <head>
 	<meta charset="utf-8" />
-	<title>자유게시판 | KHK</title>
+	<title>자유게시판수정 | KHK</title>
 	<link rel="stylesheet" href="./css/normalize.css" />
 	<link rel="stylesheet" href="./css/board.css" />
 </head>
