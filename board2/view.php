15315a529776dbe870b7d0ef2e2412900b4b4590
khk <khk0613@cside.co.kr>
2015-12-14 10:19:35 +0900

    글쓰기수정1

--- board2/view.php
+++ board2/view.php
@@ -1,8 +1,8 @@
 <?php
 	require_once("dbconfig.php");
 
-$no = $_GET['no'];
-$query = 'SELECT * FROM khk where id ='.$no;
+$no = $_GET['no']; //url뒤에 no숫자 를 붙혀주기 위한 변수
+$query = 'SELECT * FROM khk where id ='.$no; // id에 있는 모든 쿼리를 조회해서 $no에 담기
 $result = mysqli_query($dbc,$query) or die('오류');
 $row = mysqli_fetch_assoc($result);
 
@@ -27,6 +27,11 @@ $row = mysqli_fetch_assoc($result);
 							<span id="boardDate"><?php echo $row['khk_date']?></span>
 						</div>
 					<div id="boardContent"><?php echo $row['khk_article']?></div>
+				</div>
+				<div class="btnSet">
+					<a href="./write.php?no=<?php echo $no?>">수정</a>
+					<a href="./delete.php">삭제</a>
+					<a href="./">목록</a>
 				</div>
 </article>
 	</body>
--- board2/write.php
+++ board2/write.php
@@ -1,5 +1,15 @@
 <?php
 	require_once("dbconfig.php");
+$no = $_GET['no']; //url뒤에 no숫자 를 붙혀주기 위한 변수
+
+	if(isset($_GET['no'])){
+		$no = $_GET['no'];
+	}
+	if(isset($no)){
+		$query = 'SELECT * FROM khk where id ='.$no; // id에 있는 모든 쿼리를 조회해서 $no에 담기
+		$result = mysqli_query($dbc,$query) or die ('죽었습니다.');
+		$row = mysqli_fetch_assoc($result);
+	}
 ?>
 <!DOCTYPE html>
 <html>
@@ -14,12 +24,28 @@
 		<h3>자유게시판 글쓰기</h3>
 		<div id="boardWrite">
 			<form enctype="multipart/form-data" action="write_update.php" method="post">
+				<?php
+					if(isset($no)){
+						echo '<input type="hidden" name="no" value="'.$no.'">';
+					}
+
+				?>
 				<table id="boardWrite">
 					<caption class="readHide">자유게시판 글쓰기</caption>
 					<tbody>
 						<tr>
 							<th scope="row"><label for="bId">아이디</label></th>
-							<td class="id"><input type="text" name="bId" id="bId"></td>
+							<td class="id">
+								<?php 
+									if(isset($no)){
+										echo $row['id'];
+									} else{?>
+										<input type="text" name="bId" id="bId">
+									
+								<?php } ?>
+
+
+							</td>
 						</tr>
 						<tr>
 							<th scope="row"><label for="bEmail">이메일</label></th>
@@ -27,11 +53,11 @@
 						</tr>
 						<tr>
 							<th scope="row"><label for="bTitle">제목</label></th>
-							<td class="title"><input type="text" name="bTitle" id="bTitle"></td>
+							<td class="title"><input type="text" name="bTitle" id="bTitle" value="<?php echo isset($row['khk_title'])? $row['khk_title']:null?>"></td>
 						</tr>
 						<tr>
 							<th scope="row"><label for="bContent">내용</label></th>
-							<td class="content"><textarea name="bContent" id="bContent"></textarea></td>
+							<td class="content"><textarea name="bContent" id="bContent"><?php echo isset($row['b_content'])?$row['b_content']:null?></textarea></td>
 						</tr>
 						<!-- <tr>
 							<th scope="row"><label for="bContent">이미지</label></th>
@@ -40,7 +66,10 @@
 					</tbody>
 				</table>
 				<div class="btnSet">
-					<button type="submit" class="btnSubmit btn">작성</button>
+					<button type="submit" class="btnSubmit btn">
+						
+						<?php echo isset($no)?'수정':'작성'?>
+					</button>
 					<a href="index.php" class="btnList btn">목록</a>
 				</div>
 			</form>
